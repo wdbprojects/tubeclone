1baa1901002e00ba89b0ab9a901157532efb3928
MAC <ronyortiz@MACs-iMac.local>
2025-03-21 00:05:06 -0400

    fix small tRPC code

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -10,7 +10,6 @@ export const appRouter = createTRPCRouter({
     )
     .query((opts) => {
       // throw new TRPCError({ code: "BAD_REQUEST" });
-      console.log({ dbUser: opts.ctx.user });
       return {
         greeting: `hello ${opts.input.text}`,
       };
